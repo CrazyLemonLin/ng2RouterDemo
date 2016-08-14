8168e6148e3ebb55a80631bddca78d60d7428f74
CYL <lemonlinchju@hotmail.com>
2016-08-14 23:03:07 +0800

    完成routing設定，使用得三個頁面都可以切換

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,14 @@
-	import  {Routes,RouterModule} from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
+import {MessagesComponent} from './messages/messages.component';
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'messages', component: MessagesComponent }
+];
 
-const appRoutes:Routes =[];
-
-export const appRoutingProviders:any[]=[];
+export const appRoutingProviders: any[] = [];
 
 export const routing = RouterModule.forRoot(appRoutes);
