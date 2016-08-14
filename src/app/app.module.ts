901390e5a37bd161293a8da07b47e7a87630910e
CYL <lemonlinchju@hotmail.com>
2016-08-14 22:40:47 +0800

    設定好routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { appRoutingProviders,routing} from './app.routing';
 import { AppComponent } from './app.component';
 import { RouteNavComponent } from './route-nav/route-nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RouteNavComponent
+    RouteNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
